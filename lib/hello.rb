7bb89a24c2a9bf64267e6e2eea6b8fdc13522288
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-07 00:13:52 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,12 @@
-def hello_t
- if block_given?
+def hello_t(array)
+  if block_given?
     i = 0
- 
+
     while i < array.length
       yield(array[i])
       i = i + 1
     end
- 
+
     array
   else
     puts "Hey! No block was given!"
@@ -19,4 +19,3 @@ end
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
-end
